5408f179aa80413ab2a0ad75772823c6becc6e4d
Sviatoslav Bulbakha <mail@ssbb.me>
2016-02-04 22:00:55 +0500

    remove deprecated wxADJUST_MINSIZE

--- etg/defs.py
+++ etg/defs.py
@@ -12,15 +12,15 @@ import sys
 import etgtools
 import etgtools.tweaker_tools as tools
 
-PACKAGE   = "wx"   
+PACKAGE   = "wx"
 MODULE    = "_core"
 NAME      = "defs"   # Base name of the file to generate to for this script
 DOCSTRING = ""
 
 # The classes and/or the basename of the Doxygen XML files to be processed by
-# this script. 
-ITEMS  = [ 'defs_8h.xml' ]    
-    
+# this script.
+ITEMS  = [ 'defs_8h.xml' ]
+
 #---------------------------------------------------------------------------
 
 def run():
@@ -29,16 +29,16 @@ def run():
                                 check4unittest=False)
     etgtools.parseDoxyXML(module, ITEMS)
     module.check4unittest = False
-    
+
     #-----------------------------------------------------------------
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
-        
+
     # tweaks for defs.h to help SIP understand the types better
     module.find('wxInt16').type = 'short'
     module.find('wxInt64').type = 'long long'
     module.find('wxUint64').type = 'unsigned long long'
-    
+
     # NOTE: this is better, but probably still not totally correct...
     if sys.maxsize > 2**32:
         module.find('wxIntPtr').type =  'long long'           #'ssize_t'
@@ -46,16 +46,16 @@ def run():
     else:
         module.find('wxIntPtr').type =  'long'                #'ssize_t'
         module.find('wxUIntPtr').type = 'unsigned long'       #'size_t'
-        
+
     module.find('wxInt8').pyInt = True
     module.find('wxUint8').pyInt = True
     module.find('wxByte').pyInt = True
-    
+
     module.find('wxDELETE').ignore()
     module.find('wxDELETEA').ignore()
     module.find('wxSwap').ignore()
     module.find('wxVaCopy').ignore()
-    
+
     # Add some typedefs for basic wx types and others so the backend
     # generator knows what they are
     td = module.find('wxUIntPtr')
@@ -66,12 +66,12 @@ def run():
     module.insertItemAfter(td, etgtools.TypedefDef(type='long long', name='wxFileOffset'))
     module.insertItemAfter(td, etgtools.TypedefDef(type='SIP_SSIZE_T', name='ssize_t'))
     module.insertItemAfter(td, etgtools.TypedefDef(type='unsigned char', name='byte', pyInt=True))
-    
 
-    
+
+
     # Forward declarations for classes that are referenced but not defined
-    # yet. 
-    # 
+    # yet.
+    #
     # TODO: Remove these when the classes are added for real.
     # TODO: Add these classes for real :-)
     module.insertItem(0, etgtools.WigCode("""\
@@ -79,14 +79,14 @@ def run():
         class wxPalette;
         class wxExecuteEnv;
         """))
-    
-   
+
+
     # Add some code for getting the version numbers
     module.addCppCode("""
         #include <wx/version.h>
         const int MAJOR_VERSION = wxMAJOR_VERSION;
-        const int MINOR_VERSION = wxMINOR_VERSION;           
-        const int RELEASE_NUMBER = wxRELEASE_NUMBER;     
+        const int MINOR_VERSION = wxMINOR_VERSION;
+        const int RELEASE_NUMBER = wxRELEASE_NUMBER;
         """)
     module.addItem(etgtools.WigCode("""
         const int MAJOR_VERSION;
@@ -95,16 +95,15 @@ def run():
         """))
 
     module.addPyCode("BG_STYLE_CUSTOM = BG_STYLE_PAINT")
-    module.addItem(etgtools.DefineDef(name='wxADJUST_MINSIZE', value='0'))
-    
+    # module.addItem(etgtools.DefineDef(name='wxADJUST_MINSIZE', value='0'))
+
 
     #-----------------------------------------------------------------
     tools.doCommonTweaks(module)
     tools.runGenerators(module)
-    
-    
-    
+
+
+
 #---------------------------------------------------------------------------
 if __name__ == '__main__':
     run()
-
