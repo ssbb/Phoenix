a58b0e75e5215f223562ffb71dfa6d53d8dfac83
Sviatoslav Bulbakha <mail@ssbb.me>
2016-02-11 22:05:31 +0500

    trying to pass tests

--- unittests/test_intl.py
+++ unittests/test_intl.py
@@ -1,18 +1,18 @@
-import imp_unittest, unittest
-import wtc
+import unittest
+
 import wx
-import os
+
+import wtc
+
 
 #---------------------------------------------------------------------------
 
 class intl_Tests(wtc.WidgetTestCase):
-
     def test_intlLanguageInfo(self):
         info = wx.Locale.GetLanguageInfo(wx.LANGUAGE_ENGLISH_US)
         info.CanonicalName
         info.Description
         info.LocaleName
-
 
     def test_intlLocale(self):
         loc = wx.Locale(wx.LANGUAGE_ENGLISH_US)
@@ -24,14 +24,12 @@ class intl_Tests(wtc.WidgetTestCase):
         loc.SysName
         del loc
 
-
     def test_intlGetString(self):
         # This tests if we're able to pull translations from the wx message catalogs
         loc = wx.Locale(wx.LANGUAGE_SPANISH)
         st = loc.GetString('Next')
         self.assertEqual(st, 'Siguiente')
-        
-            
+
     def test_intlConstants(self):
         # just a few of them to make sure the file is included properly
         wx.LANGUAGE_AFRIKAANS
@@ -39,7 +37,7 @@ class intl_Tests(wtc.WidgetTestCase):
         wx.LANGUAGE_AMHARIC
 
 
-        
+
 #---------------------------------------------------------------------------
 
 
